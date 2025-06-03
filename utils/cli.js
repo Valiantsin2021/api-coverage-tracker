1531c3c288eb83bd64584b6ffdd8f2ebcddd5488
Lutchanka, Valentin <lutchenkovalentin@gmail.com>
2025-06-03 21:37:26 +0200

    feat: sanitize cli arguments from quotes

--- utils/cli.js
+++ utils/cli.js
@@ -16,7 +16,8 @@ function parseArgs(args) {
   for (let i = 0; i < args.length; i++) {
     if (args[i].startsWith('--')) {
       const key = args[i].slice(2)
-      params[key] = args[i + 1]
+      const value = args[i + 1].replace(/^['"]|['"]$/g, '')
+      params[key] = value
       i++
     }
   }
